1a69c4f1db77a600e369aad84a50f7e14669038c
foadyousef <fyousef@mtu.edu>
2019-09-02 18:02:27 -0600

    efficient scrper

--- scraping/allTrail_scraping.py
+++ scraping/allTrail_scraping.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
-
+import pandas as pd
 
 driver = webdriver.Firefox()
 driver.get("https://www.alltrails.com/trail/us/south-carolina/savannah-river-bluffs")
@@ -24,23 +24,34 @@ reviews = soup.find_all('div',attrs={"id":"reviews"})
 users = reviews[0].find_all('div', attrs={"feed-item"})
 
 # Dates
+date = []
 for user in users:
     dt = user.find_all('span', attrs={"subtext pull-right"})
     print(dt[0].meta['content'])
+    date.append(dt[0].meta['content'])
 
 # Comments
+coms = []
 for user in users:
     ct = user.find_all('p', attrs={'xlate-google'})
     print(ct[0].text)
+    coms.append(ct[0].text)
     
 # Tags
-    
-ctss = 0
 ts = []
 for user in users:
     tgs = user.find_all('span', attrs={'small active rounded'})
-    ctss +=1
-    for i in range(len(tgs)):
-        print(tgs[i].text)
-        t = tgs[i].text
-        ts.append(t)
+    print(len(tgs))
+    if len(tgs) != 0:
+        tl=[]
+        for i in tgs:
+            print(i.text)
+            t = i.text
+            tl.append(t)
+    else:
+        tl=[]
+        tl.append("")
+    ts.append(tl)
+
+
+df = pd.DataFrame(list(zip(date, ts, coms)), columns=['Date','Tags','Comments'])
